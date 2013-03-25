fb5ee4342273e35db686895282d11fe2f5cf5050
Wes <wes@wes.(none)>
2013-03-25 17:48:30 -0500

    Structs :)

--- company.c
+++ company.c
@@ -12,8 +12,9 @@ struct Company	{
 	int revenue;
 };
 
-struct Company *Company_create(char *name, char *product, char *location, int unit_price, int units_sold, int revenue)
+struct Company Company_create(char *name, char *product, char *location, int unit_price, int units_sold, int revenue)
 {
+	/*
 	struct Company *me = malloc(sizeof(struct Company));
 	assert(me != NULL);
 
@@ -23,36 +24,49 @@ struct Company *Company_create(char *name, char *product, char *location, int un
 	me->unit_price = unit_price;
 	me->units_sold = units_sold;
 	me->revenue = revenue;
+	*/
+	struct Company me;
+	me.name = name;
+	me.product = product;
+	me.location = location;
+	me.unit_price = unit_price;
+	me.units_sold = units_sold;
+	me.revenue = revenue;
+
+	
+	
 
 	return me;
 }
-
-void Company_destroy(struct Company *me)
+/*
+void Company_destroy(struct Company me)
 {
 	assert(me != NULL);
-	
-	free(me->name);
-	free(me->product);
-	free(me->location);
+		
+	free(me.name);
+	free(me.product);
+	free(me.location);
 	free(me);
+	
+	delete[] me;
 }
-
-void Company_print(struct Company *me)
+*/
+void Company_print(struct Company me)
 {
-	printf("My company's name is: %s.\n", me->name);
-	printf("\tMy company's product is: %s.\n", me->product);
-	printf("\tMy company is located in %s.\n", me->location);
-	printf("\tThis year we've been selling our products at %d per unit.\n", me->unit_price);
-	printf("\tThis year we sold %d units at a profit of %d.\n", me->units_sold, me->revenue);
+	printf("My company's name is: %s.\n", me.name);
+	printf("\tMy company's product is: %s.\n", me.product);
+	printf("\tMy company is located in %s.\n", me.location);
+	printf("\tThis year we've been selling our products at %d per unit.\n", me.unit_price);
+	printf("\tThis year we sold %d units at a profit of %d.\n", me.units_sold, me.revenue);
 }
 
 int main(int argc, char *argv[])
 {
-
-	struct Company *mycomp = Company_create(
+	
+	struct Company mycomp = Company_create(
 		"Devengsoft", "Web Applications", "Chicago, IL", 10000, 8, 80000);
 
-	printf("My company is at memory location %p:\n", mycomp);
+	//printf("My company is at memory location %p:\n", mycomp);
 	Company_print(mycomp);
 
 	return 0;
