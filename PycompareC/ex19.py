d4533cd552f1f87de25a212ca22111fe8244a648
Wes <wes@wes.(none)>
2013-04-12 13:32:21 -0500

    lesser monster

--- PycompareC/ex19.py
+++ PycompareC/ex19.py
@@ -3,8 +3,9 @@ import sys
 
 
 class Person(object):
-	def __init__(self):
-		self._name = 'Character'
+	def __init__(self, name):
+		self._name = name
+		self._alive = True
 		self._life = 100
 		self._power = 100
 		self._room = None
@@ -22,41 +23,54 @@ class Person(object):
 			return "You are not in a room"
 	def move(self, room):
 		if self._room != None:
-			print "Moved from %s to %s" % (self._room._room, room._room)
-			self._room._person = None
-			self._room = room
-			room._person = self
+			if room in self._room._access:
+				print "Moved from %s to %s" % (self._room._room, room._room)
+				self._room._person = None
+				self._room = room
+				room._person = self
+			else:
+				print "Cannot access %s from %s" % (room._room, self._room._room)
+				print "Here are your options: "
+				for room in self._room._access:
+					print room._room
 		else:
 			print "Moved into %s" % room._room
 			self._room = room
 			room._person = self
 
 	def attack(self, monster, damage):
-		if self.get_location() == monster.get_location():
-			if monster._life > damage:
-				monster._life -= damage
-				print "{0} attacked {1} and dealt {2} damage!".format(self._name, monster._name, damage)
-				monster.grunt()
+		if monster._alive:
+			if self.get_location() == monster.get_location():
+				if monster._life > damage:
+					monster._life -= damage
+					print "{0} attacked {1} and dealt {2} damage!".format(self._name, monster._name, damage)
+					monster.grunt()
+				else:
+					print "{0} attacked and killed the {1}!!!!".format(self._name, monster._name)
+					monster.die()
 			else:
-				print "{0} attacked and killed the {1}!!!!".format(self._name, monster._name)
-				monster.die()
+				print "You are not in the same room as {0}. {0} is in {1}".format(monster.get_name(), monster.get_location())
 		else:
-			print "You are not in the same room as {0}. {0} is in {1}".format(monster.get_name(), monster.get_location())
+			print "%s is already dead." % monster._name
 
 	def attack_response(self):
 		print "Ow, that hurt!"
 
 	def die(self):
+		self._alive = False
+		self._life = 0
 		print "%s is dead." % self._name
 
 
 class Room(object):
-	def __init__(self, location, room):
-		self._location = location
+	def __init__(self, room):
 		self._room = room
+		self._access = []
 		self._monster = None
 		self._person = None
-	
+
+	def add_room(self, room):
+		self._access.append(room)	
 
 	def has_person(self):
 		if self._person != None:
@@ -74,8 +88,9 @@ class Room(object):
 
 
 class Monster(object):
-	def __init__(self):
-		self._name = 'Ugly Monster'
+	def __init__(self, name):
+		self._name = name
+		self._alive = True
 		self._life = 50
 		self._power = 50
 		self._room = None
@@ -102,10 +117,16 @@ class Monster(object):
 
 	def move(self, room):
 		if self._room != None:
-			self._room._monster = None
-			print "{0} moved from {1} to {2}".format(self.get_name(), self.get_location(), room._room)
-			self._room = room
-			room._monster = self
+			if room in self._room._access:
+				self._room._monster = None
+				print "{0} moved from {1} to {2}".format(self.get_name(), self.get_location(), room._room)
+				self._room = room
+				room._monster = self
+			else:
+				print "Cannot move to %s from %s" % (room._room, self._room._room)
+				print "Here are your options: "
+				for room in self._room._access:
+					print room._room
 		else:
 			self._room = room
 			room._monster = self
@@ -119,12 +140,18 @@ class Monster(object):
 
 	def die(self):
 		self._life = 0
+		self._alive = False
 		print "%s is dead" % self._name
 
 
 
 
-
+class Lesser_Monster(Monster):
+	def __init__(self, name):
+		self._name = name
+		self._life = 20
+		self._power = 20
+		self._room = None
 
 
 
